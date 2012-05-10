1418c5bc1b78bfdd9652d97f81151a83c7bfa818
Raghuveera Betina <rbetina@gmail.com>
2012-05-10 07:19:29 -0500

    Restricting to only posts with videos.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,8 @@ class PostsController < ApplicationController
 
   def index
     @posts = JSON.parse(open("https://graph.facebook.com/me/home?access_token=AAACEdEose0cBAMotCf1ZArudeJ7LTiQFrxaRehy6YcrOlpLxZAZBG6mGYwRdZCNU2Pzs2H7Jq4erlN3lIk0ZAAl8fg2XMcVqUr7UyJ4YWhQZDZD&limit=100").read)["data"]
+    
+    @posts_with_videos = @posts.select { |post| post["type"] == "video" }
   end
 
 end
